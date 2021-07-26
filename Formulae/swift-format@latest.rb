c2432297807a0bb2a1e2a3dc36d6759332a71652
Stephen Celis <stephen@stephencelis.com>
2021-07-26 15:41:47 -0400

    Latest

--- Formulae/swift-format@latest.rb
+++ Formulae/swift-format@latest.rb
@@ -1,1 +1,17 @@
-Formulae/swift-format@5.4.rb
+class SwiftFormat < Formula
+    desc "`swift-format` provides the formatting technology for SourceKit-LSP and the building blocks for doing code formatting transformations."
+    homepage "https://github.com/apple/swift-format"
+    url "https://github.com/apple/swift-format.git", :branch => "main"
+    version "latest"
+
+    depends_on :xcode => ["13.0", :build]
+
+    def install
+        system "swift build -c release --disable-sandbox --build-path '.build'"
+        bin.install ".build/release/swift-format"
+    end
+
+    test do
+        system "#{bin}/swift-format --version"
+    end
+end
