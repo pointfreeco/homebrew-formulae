c66935d8de148248da7c090027fe271d44ebffdc
Stephen Celis <stephen@stephencelis.com>
2021-07-27 11:02:00 -0400

    wip

--- Formulae/swift-format@5.5.rb
+++ Formulae/swift-format@5.5.rb
@@ -1,4 +1,4 @@
-class SwiftFormatAT54 < Formula
+class SwiftFormatAT55 < Formula
     desc "`swift-format` provides the formatting technology for SourceKit-LSP and the building blocks for doing code formatting transformations."
     homepage "https://github.com/apple/swift-format"
     url "https://github.com/apple/swift-format.git", :branch => "swift-5.5-branch"
