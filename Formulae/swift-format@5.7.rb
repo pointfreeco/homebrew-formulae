adabab0f4c5f21df32c39dda81919bad70ed6059
Stephen Celis <stephen@stephencelis.com>
2022-10-10 10:40:43 -0700

    wip

--- Formulae/swift-format@5.7.rb
+++ Formulae/swift-format@5.7.rb
@@ -1,17 +1,19 @@
 class SwiftFormatAT57 < Formula
-    desc "`swift-format` provides the formatting technology for SourceKit-LSP and the building blocks for doing code formatting transformations."
-    homepage "https://github.com/apple/swift-format"
-    url "https://github.com/apple/swift-format.git", :branch => "release/5.7"
-    version "5.7"
+  desc "`swift-format` provides the formatting technology for SourceKit-LSP and the building blocks for doing code formatting transformations."
+  homepage "https://github.com/apple/swift-format"
+  url "https://github.com/apple/swift-format.git",
+    :tag => "0.50700.1",
+    :revision => "5f184220d032a019a63df457cdea4b9c8241e911"
+  version "5.7"
 
-    depends_on :xcode => ["14.0", :build]
+  depends_on :xcode => ["14.0.1", :build]
 
-    def install
-        system "swift build -c release --disable-sandbox --build-path '.build'"
-        bin.install ".build/release/swift-format"
-    end
+  def install
+    system "swift build -c release --disable-sandbox --build-path '.build'"
+    bin.install ".build/release/swift-format"
+  end
 
-    test do
-        system "#{bin}/swift-format --version"
-    end
+  test do
+    system "#{bin}/swift-format --version"
+  end
 end
